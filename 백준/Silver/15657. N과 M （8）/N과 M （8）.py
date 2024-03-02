d2a8cc5c33ab846737bc1d2d3d0bbb62cd0a8a58
Youn-Rha <86452280+Youn-Rha@users.noreply.github.com>
2024-03-03 03:20:21 +0900

    [Silver III] Title: N과 M (8), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,20 @@
+import sys
+
+def input():
+	return sys.stdin.readline()
+	
+
+def sol(k):
+	if k == M:
+		print(" ".join(map(str, ans)))
+		return
+	for i in range(lst.index(ans[k - 1]), N):
+		ans[k] = lst[i]
+		sol(k + 1)
+	
+	
+	
+N, M = map(int, input().split())
+lst = sorted(list(map(int, input().split())))
+ans = [lst[0]] * M
+sol(0)
