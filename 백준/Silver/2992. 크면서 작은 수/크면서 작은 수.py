cafc373231795735d6654351e17b41ed6cebb511
Youn-Rha <86452280+Youn-Rha@users.noreply.github.com>
2024-03-10 23:32:33 +0900

    [Silver III] Title: 크면서 작은 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
@@ -7,7 +7,7 @@ def input():
 
 def sol(k):
     if k == len(X):
-        num.append(int("".join(ans)))
+        num.append(int("".join(ans)))  # 문자열을 정수형으로 정답배열에 추가
         return
     for i in range(len(X)):
         if not visited[i]:
@@ -23,9 +23,9 @@ if __name__ == "__main__":
     visited = [False] * len(X)
     num = []
     sol(0)
-    num.sort(reverse=True)
-    idx = num.index(int(X))
-    if idx != 0:
+    num.sort(reverse=True)              # 정수형으로 저장된 숫자들을 내림차순 정렬
+    idx = num.index(int(X))             # X의 바로 앞 문자를 찾기 위한 X의 index 추출
+    if idx != 0:                        # idx 0인 경우는 X가 맨 앞에 있을 경우, 즉 X가 가장 큰 경우
         print(num[idx - 1])
     else:
         print(0)
