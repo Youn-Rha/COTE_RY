5428f57fe73d27517230b958d6e82cb86f954e4a
Youn-Rha <86452280+Youn-Rha@users.noreply.github.com>
2024-03-14 13:31:19 +0900

    [Silver V] Title: 거스름돈, Time: 100 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline()
+
+
+# main
+if __name__ == "__main__":
+    n = int(input())
+    dp = [-1, -1, 1, -1, 2, 1] + [-1] * 99_995          # 예외 case 미리 설정 후 배열 설정
+    for i in range(6, n + 1):
+        if dp[i - 5] != -1 and dp[i - 2] != -1:         # 이전 단계에서 5원을 추가할 수 있는 경우와 2원을 추가할 수 있는 경우 비교
+            dp[i] = min(dp[i - 5] + 1, dp[i - 2] + 1)
+        elif dp[i - 5] == -1 and dp[i - 2] != -1:       # 이전 단계에서 2원만 추가할 수 있는 경우만 존재할 경우
+            dp[i] = dp[i - 2] + 1
+        else:                                           # 이전 단계에서 5원만 추가할 수 있는 경우만 존재할 경우
+            dp[i] = dp[i - 5] + 1
+    print(dp[n])
