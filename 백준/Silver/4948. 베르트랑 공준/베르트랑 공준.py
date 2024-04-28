f64e7e08161bd1aecb639eb87e1e14a0c68d1734
Youn-Rha <86452280+Youn-Rha@users.noreply.github.com>
2024-04-29 02:09:13 +0900

    [Silver II] Title: 베르트랑 공준, Time: 184 ms, Memory: 36888 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,31 +1,13 @@
-import sys
+import math
+n = int(input())
 
-sys.setrecursionlimit(100000)
+d = [1]*(2*123456+1)
 
+for i in range(2, int(math.sqrt(123456*2+1))):
+    if d[i]:
+        for j in range(i+i, len(d), i):
+            d[j] = 0
 
-def input():
-    return sys.stdin.readline()
-
-
-def primeSieve(N):
-    prime = [True] * (N + 1)
-    prime[0], prime[1] = False, False
-    p = 2
-    while p * p <= N + 1:
-        prime[p * p::p] = [False] * ((N - p * p) // p + 1)
-        p += 1
-    return prime
-
-
-# main
-if __name__ == "__main__":
-    while True:
-        N = int(input())
-        if N == 0:
-            break
-        prime = primeSieve(2 * N + 1)
-        ans = []
-        for i in range(N + 1, 2 * N + 1):
-            if prime[i]:
-                ans.append(i)
-        print(len(ans))
+while n!=0:
+    print(d[n+1:2*n+1].count(1))
+    n = int(input())
