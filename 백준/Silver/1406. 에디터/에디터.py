97b9bc3a7f3abf67fca439fd9ddc0b508059ee09
Youn-Rha <86452280+Youn-Rha@users.noreply.github.com>
2025-02-01 20:30:58 +0900

    [Silver II] Title: 에디터, Time: 340 ms, Memory: 42600 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -9,27 +9,21 @@ def input():
 
 # main
 if __name__ == "__main__":
-    # left ^ cursor ^ right
-    left = collections.deque(" " + " ".join(input().rstrip()) + " ")
+    left = collections.deque((input().rstrip()))
     right = collections.deque()
-    # abcd -> _a_b_c_d_ -> 중간에 공백으로 커서 위치 판별
     n = int(input())
     for _ in range(n):
         w = "".join(input().split())
         if w[0] == 'L':
-            if len(left) >= 2:
-                right.appendleft(left.pop())
+            if len(left) >= 1:
                 right.appendleft(left.pop())
         elif w[0] == 'D':
-            if len(right) >= 2:
-                left.append(right.popleft())
+            if len(right) >= 1:
                 left.append(right.popleft())
         elif w[0] == 'B':
-            if len(left) >= 2:
-                left.pop()
+            if len(left) >= 1:
                 left.pop()
         elif w[0] == 'P':
             left.append(w[1])
-            left.append(" ")
 
-    print("".join(left).replace(" ", "") + "".join(right).replace(" ", ""))
+    print("".join(left) + "".join(right))
