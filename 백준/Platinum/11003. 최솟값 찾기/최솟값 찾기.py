8003cdbc3e5c633a97a9bf3f5f4b36da2ee535d2
Youn-Rha <86452280+Youn-Rha@users.noreply.github.com>
2025-02-03 02:46:33 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2492 ms, Memory: 683624 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     deque = collections.deque()
     lst = list(map(int, input().split()))
     for i, l in enumerate(lst):
-        while deque and deque[-1] > l:
-            deque.pop()
+        while deque and deque[-1] > l:      # 자신보다 크면 다 지움
+            deque.pop()                     # 덱의 맨 앞에는 최솟값이 옴
         deque.append(l)
-        if i >= L and deque[0] == lst[i - L]:
-            deque.popleft()
+        if i >= L and deque[0] == lst[i - L]:   # 덱의 맨 앞 원소가
+            deque.popleft()                     # 지워야 하면 지움
         print(deque[0], end=" ")
