f8aba20e1415dc3393e402ebb061f91293bfac6e
Youn-Rha <86452280+Youn-Rha@users.noreply.github.com>
2025-03-03 19:54:16 +0900

    [Silver II] Title: 제곱수의 합, Time: 188 ms, Memory: 111112 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -9,13 +9,13 @@ def input():
 if __name__ == "__main__":
     N = int(input())
     dp = [float('inf')] * (N + 1)
-    dp[1] = 1
-    for i in range(2, N + 1):
+    for i in range(1, N + 1):
         p = int(i ** 0.5)
         if p ** 2 == i:
             dp[i] = 1
         else:
             while p != 0:
-                dp[i] = min(dp[i], dp[i - p ** 2] + dp[p ** 2])
+                # dp[i - p ** 2] + dp[p ** 2] 원래 이렇게 생각했는데 뒤에꺼는 무조건 제곱수라 1임
+                dp[i] = min(dp[i], dp[i - p ** 2] + 1) 
                 p -= 1
     print(dp[N])
