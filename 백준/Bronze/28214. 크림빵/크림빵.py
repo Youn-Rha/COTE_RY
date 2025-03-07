50bd7bcc8a53fc58c8e85a010170eeda79416856
Youn-Rha <86452280+Youn-Rha@users.noreply.github.com>
2025-03-07 16:46:40 +0900

    [Bronze III] Title: 크림빵, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.py
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     lst = list(map(int, input().split()))
     cnt = 0
     for i in range(N):
-        cream = 0
+        nocream = 0
         for j in range(K):
-            if lst[i * K + j] == 1:
-                cream += 1
-        if cream >= P:
+            if lst[i * K + j] == 0:
+                nocream += 1
+        if nocream < P:
             cnt += 1
     print(cnt)
