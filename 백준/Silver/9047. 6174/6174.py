4c355704067be085a5892ea2d3c6dc623adf17a7
Youn-Rha <86452280+Youn-Rha@users.noreply.github.com>
2025-12-03 18:40:08 +0900

    [Silver V] Title: 6174, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9047. 6174/6174.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline()
+
+
+# main
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        n = input().rstrip()
+        cnt = 0
+        while True:
+            if n == "6174":
+                break
+            max_n = "".join(sorted(n, reverse=True))
+            min_n = max_n[::-1]
+            n = str(int(max_n) - int(min_n))
+            if len(n) < 4:
+                n += '0' * (4 - len(n))
+            cnt += 1
+        print(cnt)
