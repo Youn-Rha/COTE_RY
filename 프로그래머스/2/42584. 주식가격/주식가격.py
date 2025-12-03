21370a72c8798905397825372d8d07baf86292f4
Youn-Rha <86452280+Youn-Rha@users.noreply.github.com>
2025-12-04 01:51:37 +0900

    [level 2] Title: 주식가격, Time: 25.58 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,8 +1,17 @@
 def solution(prices):
     answer = [0] * len(prices)
+    stack = []
     for i in range(len(prices)):
-        for j in range(i + 1, len(prices)):
-            answer[i] += 1
-            if prices[i] > prices[j]:
-                break
+        
+        # 스택이 비어 있지 않고, 이번 시점에서 가격이 내려간 경우
+        while stack and prices[stack[-1]] > prices[i]:
+            idx = stack.pop()       # 가격이 내려간 idx 추출
+            answer[idx] = i - idx   # 내려갈 때까지 버튼 기간 입력
+
+        stack.append(i)
+        
+    # 끝까지 버틴 가격들
+    while stack:
+        idx = stack.pop()
+        answer[idx] = len(prices) - idx - 1
     return answer
